cf1b1a5e03cfd089315215e57da1346fc7eca338
Eduardo Neto <eduardo_neto@epam.com>
2020-12-21 14:06:51 +0000

    App component and index.js  update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './_library.css'
 import './global.scss'
 
+
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('app'),
 )
