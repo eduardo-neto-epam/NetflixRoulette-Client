789a676a30e84f6327e1bcd80525fa43579f5783
Eduardo Neto <eduardo_neto@epam.com>
2020-12-21 13:54:28 +0000

    Button component

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,8 +1,10 @@
 import '@testing-library/react'
+import renderer from 'react-test-renderer';
 import React from 'react'
 import ReactDOM  from 'react-dom'
 import { act } from 'react-dom/test-utils';
 import Button from './index'
+import { fireEvent } from '@testing-library/react';
 
 let container;
 
@@ -16,7 +18,14 @@ afterEach(() => {
   container = null;
 });
 
-test('button label renders according to prop', () => {
+test('Button renders correctly', ()=>{
+  const tree = renderer
+    .create(<Button size='lg' style='glass'>+ ADD MOVIE</Button>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+test('button renders with text given as child', () => {
 
     const label = 'Click Me'
 
@@ -25,4 +34,55 @@ test('button label renders according to prop', () => {
     const btn = container.querySelector('button');
 
     expect(btn.textContent).toBe(label)
+})
+
+test('button takes class according to size props', () => {
+
+  const sizeValues = [ 'sm', 'md', 'lg' ]
+  
+  function testSize(item) {
+    act(() => { ReactDOM.render(<Button size={item}></Button>, container) });
+
+    let btn = container.querySelector('button');
+
+    expect(btn.getAttribute('class')).toMatch(`btn btn--${item} btn--primary`);
+  }
+  
+  sizeValues.forEach(testSize)
+
+})
+test('button takes class according to style props', () => {
+
+  const styleValues = [ 'primary', 'outline', 'glass' ]
+  
+  function testStyle(item) {
+    act(() => { ReactDOM.render(<Button style={item}></Button>, container) });
+
+    let btn = container.querySelector('button');
+
+    expect(btn.getAttribute('class')).toMatch(`btn btn--md btn--${item}`);
+  }
+  
+  styleValues.forEach(testStyle)
+
+})
+
+test('simulates click', () => {
+
+  const mockedClick = jest.fn()
+
+  function testClick() {
+
+    act(() => { ReactDOM.render(<Button handleClick={mockedClick}></Button>, container) });
+
+    let btn = container.querySelector('button');
+
+    fireEvent.click(btn);
+
+    expect(mockedClick.mock.calls.length).toEqual(1)
+
+  }
+
+  testClick();
+
 })
