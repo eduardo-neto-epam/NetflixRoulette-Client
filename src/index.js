d88f1d3318f50df4db81726a9d22fbbd5fdbb690
Eduardo Neto <eduardo_neto@epam.com>
2020-12-29 13:59:21 +0000

    added root element variable

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from './App'
 import './_library.css'
 import './global.scss'
 
-
+const rootElement = document.getElementById("app");
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('app'),
+  rootElement,
 )
